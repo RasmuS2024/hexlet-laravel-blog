d2b9e597beec18ce1e1d008b21aaf6a673634a50
RasmuS2024 <rasburkhanov@yandex.ru>
2025-04-13 22:29:30 +0300

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
+Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
 
+/*
 Route::get('about', function () {
     return view('about');
 })->name('about');
+*/
