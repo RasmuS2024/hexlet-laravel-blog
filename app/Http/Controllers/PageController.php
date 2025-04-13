78f6aa07777eeae8d00c3f1a0ad124a271a9609b
RasmuS2024 <rasburkhanov@yandex.ru>
2025-04-13 18:48:38 +0300

    added controller PageController

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function about()
+    {
+        // Точка используется вместо /.
+        // То есть шаблон находится по пути resources/views/page/about.blade.php
+        return view('page.about');
+    }
+
+    public function welcome()
+    {
+        // Точка используется вместо /.
+        // То есть шаблон находится по пути resources/views/page/about.blade.php
+        return view('page.welcome');
+    }
+}
