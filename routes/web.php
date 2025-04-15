393c4a5d2900cad66e644530fbafd6470f378fd8
RasmuS2024 <rasburkhanov@yandex.ru>
2025-04-15 22:45:20 +0300

    new file:   app/Http/Controllers/ArticleController.php, resources/views/article/index.blade.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('about', [PageController::class, 'about'])->name('about');
 Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+
+
+Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
 
 /*
 Route::get('/', function () {
