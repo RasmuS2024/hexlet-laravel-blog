7487f9e2ac80bcf60eefdfb3ff5bae387efc193b
RasmuS2024 <rasburkhanov@yandex.ru>
2025-04-20 19:09:53 +0300

    modified files for edit form

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.app')
 
+@if ($errors->any())
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+@if(session()->has('success'))
+  <div class="alert alert-success  role="alert>
+    {{ session()->get('success') }}
+  </div>
+@endif
+
+@if(session()->has('error'))
+  <div class="alert alert-danger">
+    {{ session()->get('error') }}
+  </div>
+@endif
+
 @section('content')
     <h1>Список статей2</h1>
     @foreach ($articles as $article)
         <h2><a href="articles/{{$article->id}}">{{$article->name}}</a></h2>
+        <h4><a href="articles/{{$article->id}}/edit">Редактировать</a></h4>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,16 @@ Route::get('/', [PageController::class, 'welcome'])->name('welcome');
 
 Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
 
+Route::get('articles/create', [ArticleController::class, 'create'])
+  ->name('articles.create');
+
+Route::post('articles', [ArticleController::class, 'store'])
+  ->name('articles.store');
+
 Route::get('articles/{id}', [ArticleController::class, 'show'])->name('articles.show');
 
+Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])
+  ->name('articles.edit');
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
-/*
-Route::get('about', function () {
-    return view('about');
-})->name('about');
-*/
+Route::patch('articles/{id}', [ArticleController::class, 'update'])
+  ->name('articles.update');
